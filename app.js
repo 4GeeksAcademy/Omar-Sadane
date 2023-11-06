490eabc2765030beeaf7e20e9c240c8df00286ba
omar saadane el meski <146116741+OmarSaadane@users.noreply.github.com>
2023-11-06 20:43:03 +0000

    js bucle anidado

---
+++ app.js
@@ -0,0 +1,11 @@
+let pronouns = ['the', 'our'];
+let adjectives = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+
+for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+            console.log(pronouns[i] + adjectives[j] + nouns[k] + '.com .net, .us, .io,');
+        }
+    }
+}
