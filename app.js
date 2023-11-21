fea7b9996a2b8a38edd84a19985493893cdf5d9f
omar saadane el meski <146116741+OmarSaadane@users.noreply.github.com>
2023-11-21 19:15:19 +0000

    domain generatos rectificado

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronouns = ['the', 'our'];
 let adjectives = ['great', 'big'];
 let nouns = ['jogger', 'racoon'];
-
+let host = ['.com', '.net', '.us', '.io']
 for (let i = 0; i < pronouns.length; i++) {
     for (let j = 0; j < adjectives.length; j++) {
         for (let k = 0; k < nouns.length; k++) {
-            console.log(pronouns[i] + adjectives[j] + nouns[k] + '.com .net, .us, .io,');
+            for (let h = 0; h < host.length;h++){
+
+                console.log(pronouns[i] + adjectives[j] + nouns[k] + host[h]);
+            }
         }
     }
 }
